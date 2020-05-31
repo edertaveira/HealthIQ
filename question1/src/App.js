7b3def748504535e55d07fd16dc82c4e266db8a2
Eder Taveira <edertaveira@gmail.com>
2020-05-31 14:55:11 -0300

    Updating the name of the component.

--- question1/src/App.js
+++ question1/src/App.js
@@ -1,9 +1,23 @@
 import React, { useState } from "react";
 import "./App.scss";
-import PoorlyWrittenTodoList from "./PoorlyWrittenTodoList";
+import TodoList from "./TodoList";
 
 function App() {
-  const [todos, setTodos] = useState(null);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      name: "Todo 1",
+      isHighlighted: true,
+      isActive: true,
+      items: ["To Finish this challenge", "To Test The app"],
+    },
+    {
+      id: 2,
+      name: "Todo 2",
+      isHighlighted: true,
+      isActive: true,
+    },
+  ]);
 
   const onHighlight = (id) => {
     setTodos(
@@ -14,7 +28,7 @@ function App() {
     );
   };
 
-  return <PoorlyWrittenTodoList onHighlight={onHighlight} todos={todos} />;
+  return <TodoList onHighlight={onHighlight} todos={todos} />;
 }
 
 export default App;
--- question1/src/TodoList.js
+++ question1/src/TodoList.js
@@ -1,28 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const PoorlyWrittenTodoList = ({ todos, onHighlight }) => {
+const TodoList = ({ todos, onHighlight }) => {
   return (
     <div>
       <h1>To Do List</h1>
-      {todos
+      {todos &&
+        todos
           .filter((t) => t.isActive)
           .map((t, idx) => (
             <div key={idx}>
               <h1>{t.name}</h1>
-              <ul>
-                {t.items && t.items.map((i) => (
-                  <li key={i}>{i}</li>
-                ))}
-              </ul>
+              <ul>{t.items && t.items.map((i) => <li key={i}>{i}</li>)}</ul>
               <button onClick={() => onHighlight(t.id)}>{t.isHighlighted ? "Unhighlight" : "Highlight"}</button>
             </div>
           ))}
     </div>
   );
 };
-PoorlyWrittenTodoList.propTypes = {
+TodoList.propTypes = {
   todos: PropTypes.array.isRequired,
   onHighlight: PropTypes.func.isRequired,
 };
-export default PoorlyWrittenTodoList;
+export default TodoList;

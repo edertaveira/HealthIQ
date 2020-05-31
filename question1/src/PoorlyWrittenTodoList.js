171672378b2adc1b18e417eb0d0ec32bef0714ad
Eder Taveira <edertaveira@gmail.com>
2020-05-31 13:12:45 -0300

    Updating the projet to simulate the issue.

--- question1/src/PoorlyWrittenTodoList.js
+++ question1/src/PoorlyWrittenTodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
 class PoorlyWrittenTodoList extends Component {
   constructor(props) {
     super(props);
--- question1/src/reducers/actions/commonActions.js
+++
@@ -1,7 +0,0 @@
-/* ACTION TYPES */
-export const SEARCH = 'SEARCH';
-
-/* ACTION CREATORS */
-export function searching(value) {
-  return { type : SEARCH, value }
-}
